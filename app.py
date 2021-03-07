43414d1bf9c1ad39b40e492fd7b7d15d34d3dbfa
josephoregon <josephoregon@icloud.com>
2021-03-06 21:19:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,44 +9,15 @@ st.set_page_config(
     page_icon='🗞'
 )
 
-st.markdown(
-    """
+st.markdown("""
 <style>
-.reportview-container .markdown-text-container {
-    font-family: monospace;
+body {
+  background: #ff0099; 
+  background: -webkit-linear-gradient(to right, #ff0099, #493240); 
+  background: linear-gradient(to right, #ff0099, #493240); 
 }
-.sidebar .sidebar-content {
-    background-image: linear-gradient(#2e7bcf,#2e7bcf);
-    color: white;
-}
-.Widget>label {
-    color: white;
-    font-family: monospace;
-}
-[class^="st-b"]  {
-    color: white;
-    font-family: monospace;
-}
-.st-bb {
-    background-color: transparent;
-}
-.st-at {
-    background-color: #0c0080;
-}
-footer {
-    font-family: monospace;
-}
-.reportview-container .main footer, .reportview-container .main footer a {
-    color: #0c0080;
-}
-header .decoration {
-    background-image: none;
-}
-
 </style>
-""",
-    unsafe_allow_html=True,
-)
+    """, unsafe_allow_html=True)
 
 
 
