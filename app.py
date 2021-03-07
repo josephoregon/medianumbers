46f9723c0441603bfa971656d778627e23bebaab
josephoregon <josephoregon@icloud.com>
2021-03-06 21:18:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,47 @@ st.set_page_config(
     page_icon='🗞'
 )
 
+st.markdown(
+    """
+<style>
+.reportview-container .markdown-text-container {
+    font-family: monospace;
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+.Widget>label {
+    color: white;
+    font-family: monospace;
+}
+[class^="st-b"]  {
+    color: white;
+    font-family: monospace;
+}
+.st-bb {
+    background-color: transparent;
+}
+.st-at {
+    background-color: #0c0080;
+}
+footer {
+    font-family: monospace;
+}
+.reportview-container .main footer, .reportview-container .main footer a {
+    color: #0c0080;
+}
+header .decoration {
+    background-image: none;
+}
+
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+
 nltk.download('stopwords')
 
 title = st.title("Media-Numbers")
